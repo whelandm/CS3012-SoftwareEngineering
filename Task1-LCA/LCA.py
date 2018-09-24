71d7987d999771dba3f8feb7683999637c24e275
whelandm <37048361+whelandm@users.noreply.github.com>
2018-09-24 20:54:43 +0100

    Yeet

---
+++ Task1-LCA/LCA.py
@@ -0,0 +1,14 @@
+# Bitch what the fuck
+
+#Node class for Binary Tree
+class Node:
+    def __init__(self, val):
+        self.left = null
+        self.right = null
+        self.value = val
+
+    def add():
+
+
+# Lowest Common Ancestor - find lowest common ancester of x and y
+def LCA(node, x, y):
