110596a1805b7eb0efbfc21a7b1838802bf11781
whelandm <37048361+whelandm@users.noreply.github.com>
2018-09-30 14:06:12 +0100

    Test for None cases

--- Task1-LCA/LCA_test.py
+++ Task1-LCA/LCA_test.py
@@ -15,3 +15,8 @@ def test_LCA():
     assert LCA.LCA(LCA.testTree, 10, 3).value == 1
     assert LCA.LCA(LCA.testTree, 5, 6).value == 5
     assert LCA.LCA(LCA.testTree, 1, 6).value == 1
+
+def test_none_LCA():
+    assert LCA.LCA(LCA.testTree, None, 1).value == 1
+    test = LCA.Node(None)
+    assert LCA.LCA(test, None, None).value == None
