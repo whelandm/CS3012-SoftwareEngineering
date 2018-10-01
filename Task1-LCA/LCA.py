14a9f3783fefb5468c0d80dfb3484220bd86b529
whelandm <37048361+whelandm@users.noreply.github.com>
2018-10-01 21:36:42 +0100

    Added additional function for errors

--- Task1-LCA/LCA.py
+++ Task1-LCA/LCA.py
@@ -8,7 +8,7 @@ class Node:
         self.right = None
         self.value = val
 
-    #Print the Binary Tree
+    #Print the Binary Tree to console
     def print_tree(self):
         if self.left != None:
             self.left.print_tree()
@@ -16,23 +16,26 @@ class Node:
         if self.right != None:
             self.right.print_tree()
 
-# Lowest Common Ancestor - find lowest common ancester of x and y
+# Lowest Common Ancestor - find lowest common ancester of x and y using recursion
 def LCA(node, x, y):
     if node is None:
         return None
+
     if node.value == x or node.value == y:
         return node
+
     left = LCA(node.left, x, y)
     right = LCA(node.right, x, y)
 
     if left != None and right != None:
         return node
+
     if left == None:
         return right
     else:
         return left
 
-#Create Tree for Testing
+#Create Binary Tree for testing
 testTree = Node(1)
 testTree.left = Node(2)
 testTree.right = Node(3)
--- Task1-LCA/LCA_test.py
+++ Task1-LCA/LCA_test.py
@@ -9,7 +9,16 @@ def test_print_tree(capsys):
     captured = capsys.readouterr()
     assert captured.out == "8\n2\n9\n7\n10\n1\n4\n3\n5\n6\n"
 
-# test lowest common ancestor
+def test_node_exists():
+    assert LCA.node_exists(LCA.testTree, 22) == False
+    assert LCA.node_exists(LCA.testTree, None) == False
+    assert LCA.node_exists(LCA.testTree, 1) == True
+    assert LCA.node_exists(LCA.testTree, 6) == True
+    assert LCA.node_exists(LCA.testTree, 8) == True
+    assert LCA.node_exists(LCA.testTree, 10) == True
+    assert LCA.node_exists(LCA.testTree, 4) == True
+
+# test lowest common ancestor with valid values
 def test_LCA():
     assert LCA.LCA(LCA.testTree, 8, 9).value == 2
     assert LCA.LCA(LCA.testTree, 10, 3).value == 1
@@ -18,16 +27,22 @@ def test_LCA():
     assert LCA.LCA(LCA.testTree, 9, 6).value == 1
     assert LCA.LCA(LCA.testTree, 4, 6).value == 3
 
-# test various cases with None Values
-def test_none_LCA():
-    assert LCA.LCA(LCA.testTree, None, 1).value == 1
-    assert LCA.LCA(LCA.testTree, 1, None).value == 1
-    assert LCA.LCA(LCA.testTree, None, None) == None
+# test lowest common ancestor with non-present values
+def test_LCA():
+    assert LCA.LCA(LCA.testTree, 10, 16) == -1
+    assert LCA.LCA(LCA.testTree, 22, 0) == -1
+    assert LCA.LCA(LCA.testTree, 22, 2) == -1
 
-# test various cases with None Node
+# test various cases with None values
+def test_none_LCA():
+    assert LCA.LCA(LCA.testTree, None, 1) == -1
+    assert LCA.LCA(LCA.testTree, 1, None) == -1
+    assert LCA.LCA(LCA.testTree, None, None) == -1
+
+# test various cases with None Node and None values
 def test_none_node_LCA():
     test = LCA.Node(None)
     assert LCA.LCA(test, None, None).value == None
-    assert LCA.LCA(test, None, 1).value == None
-    assert LCA.LCA(test, 1, None).value == None
-    assert LCA.LCA(test, 1, 1) == None
+    assert LCA.LCA(test, None, 1) == -1
+    assert LCA.LCA(test, 1, None) == -1
+    assert LCA.LCA(test, 1, 1) == -1
