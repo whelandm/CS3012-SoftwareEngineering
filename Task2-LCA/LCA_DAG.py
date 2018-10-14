55a1045a2739be751bf3ffb99fd77fd9e5d5b0b5
whelandm <37048361+whelandm@users.noreply.github.com>
2018-10-14 12:56:09 +0100

    BFS

--- Task2-LCA/LCA_DAG.py
+++ Task2-LCA/LCA_DAG.py
@@ -2,6 +2,28 @@
 
 from collections import defaultdict #unordered lists
 
+# Graph for Testing
+testDAG = {     1 : set([2, 3]),
+                2 : set([8]),
+                3 : set([9]),
+                4 : set([5, 8, 9]),
+                5 : set([6, 7]),
+                6 : set([8]),
+                7 : set([9]),
+                8 : set([]),
+                9 : set([])        }
+
+#Breath-First Search - return all ancestors of x from graph g
+def BFS(g, root, x):
+    queue = [(root, [root])]
+    while queue:
+        (vertex, path) = queue.pop(0)
+        for next in g[vertex] - set(path):
+            if next == x:
+                yield path + [next]
+            else:
+                queue.append((next, path + [next]))
+
 #Directed Acyclic Graph
 class DAG:
     #Init
@@ -14,8 +36,3 @@ class DAG:
         self.graph[edge].append((vertices,weight))
 
 #Lowest Common Ancestor - find lowest common ancester of x and y
-def LCA_DAG:
-
-
-#Create DAG for testing
-dag = DAG()
--- Task2-LCA/LCA_DAG_test.py
+++ Task2-LCA/LCA_DAG_test.py
@@ -2,3 +2,10 @@
 
 import pytest
 import LCA_DAG
+
+## coverage py.test --cov lca_dag.py test
+
+# check printTree works
+def test_BFS():
+    result = LCA_DAG.BFS(LCA_DAG.testDAG, 1, 9)
+    assert list(result) == [[1, 3, 9]]
