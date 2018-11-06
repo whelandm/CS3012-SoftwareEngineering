5498e3cb47008b671ddabdfccc2e2754b5690e99
whelandm <37048361+whelandm@users.noreply.github.com>
2018-11-06 19:28:52 +0000

    added functions to display data

--- Task5-Github Access/GithubAPI.py
+++ Task5-Github Access/GithubAPI.py
@@ -1,4 +1,7 @@
 from github import Github
+import os
+import numpy as np
+import matplotlib.pyplot as plt
 
 # return count of stargazers for all repos
 def stargazers(g, u):
@@ -11,3 +14,17 @@ def stargazers(g, u):
             labels.append(label)
             count.append(current)
     return labels, count
+
+#function to display data as pie chart
+def displayPie(data, labels):
+    plt.figure(figsize=(20,20))
+    plt.pie(data, labels=labels, autopct='%1.1f%%', startangle=0)
+    plt.axis('equal')
+    plt.show()
+#function to display data as bar chart
+def displayBar(data, labels, title):
+    y_pos = np.arange(len(data))
+    plt.bar(y_pos, data, align='center', alpha=0.5)
+    plt.xticks(y_pos, labels)
+    plt.title(title)
+    plt.show()
